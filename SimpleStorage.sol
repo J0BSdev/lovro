210d955093afc00ec31d96ab003a0cb3d51ecc77
Lovro <lovro.posel79@gmail.com>
2026-01-18 12:43:36 +0100

    update

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,43 +1,39 @@
-//SPDX-License-Identifier:MIT
-pragma solidity ^0.8.19;
+// I'm a comment!
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+// pragma solidity ^0.8.0;
+// pragma solidity >=0.8.0 <0.9.0;
 
 contract SimpleStorage {
-  //favoriteNumber gets initialized to 0 if no value is given
+    uint256 myFavoriteNumber;
 
- uint256 public myFavoriteNumber; //0
+    struct Person {
+        uint256 favoriteNumber;
+        string name;
+    }
+    // uint256[] public anArray;
+    Person[] public listOfPeople;
 
-//uint256[] listOfFavoriteNumbers;
-struct Person {
-    uint256 favoriteNumber;
-    string name;
-}
-// dynamic array
- //static array
-Person[] public listOfPeople; //empty array []
+    mapping(string => uint256) public nameToFavoriteNumber;
 
-
-mapping(string => uint256) public nameToFavoriteNumber;                
-
-// Person public pat = Person({favoriteNumber:7, name: "Pat"});
-//person public mariah=Person({favoriteNumber:10, name: "Mariah"});;
-
- function store (uint256 _favoriteNumber) public {
-     myFavoriteNumber = _favoriteNumber;
-    
- }
-
-//view ,pure
-function retrieve() public view returns(uint256){
-    return myFavoriteNumber;
-}
-//calldata,memory,storage
-    function addPerson( string memory _name, uint256 _favoriteNumber) public {
-        listOfPeople.push(Person(_favoriteNumber, _name));
-
-        nameToFavoriteNumber[_name] = _favoriteNumber;
+    function store(uint256 _favoriteNumber) public virtual {
+        myFavoriteNumber = _favoriteNumber;
     }
 
+    function retrieve() public view returns (uint256) {
+        return myFavoriteNumber;
+    }
 
-
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push(Person(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
 }
 
+contract SimpleStorage2 {}
+
+contract SimpleStorage3 {}
+
+contract SimpleStorage4 {}
