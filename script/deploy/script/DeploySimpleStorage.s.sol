ff31446e0a296b8c1b6d67d26f49bb9f07befd04
Lovro <lovro.posel79@gmail.com>
2026-01-20 20:20:31 +0100

    update

--- script/deploy/script/DeploySimpleStorage.s.sol
+++ script/deploy/script/DeploySimpleStorage.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
-import {SimpleStorage} from "../../SimpleStorage.sol";
+import {SimpleStorage} from "../../../src/SimpleStorage.sol";
 
 contract DeploySimpleStorage is Script {
     function run() external returns (SimpleStorage) {
