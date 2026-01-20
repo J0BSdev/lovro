47c8c9d4d773baaf3a291cb6603a1a50a6c60006
Lovro <lovro.posel79@gmail.com>
2026-01-20 21:14:15 +0100

    update

--- src/SimpleStorage.sol
+++ src/SimpleStorage.sol
@@ -6,7 +6,7 @@ pragma solidity ^0.8.18;
 // pragma solidity ^0.8.0;
 // pragma solidity >=0.8.0 <0.9.0;
 
-contract SimpleStorag {
+contract SimpleStorage {
     uint256 myFavoriteNumber;
 
     struct Person {
