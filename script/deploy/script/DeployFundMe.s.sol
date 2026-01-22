e2ff0f76adc1a86208df8333345a5df37abc1ad5
Lovro <lovro.posel79@gmail.com>
2026-01-22 20:08:57 +0100

    update

--- script/deploy/script/DeployFundMe.s.sol
+++ script/deploy/script/DeployFundMe.s.sol
@@ -1,15 +1,22 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
 
 import {Script} from "forge-std/Script.sol";
-import {FundMe} from "../../../src/FundMe.sol"; 
+import {Helperconfig} from "./Helperconfig.s.sol";  
+import {FundMe} from "../../../src/FundMe.sol";
 
 contract DeployFundMe is Script {
-    function run() external returns (FundMe) {
+    function deployFundMe() public returns (FundMe, HelperConfig) {
+        HelperConfig helperConfig = new HelperConfig(); // This comes with our mocks!
+        address priceFeed = helperConfig.getConfigByChainId(block.chainid).priceFeed;
+
         vm.startBroadcast();
-        
-        FundMe fundMe = new FundMe();(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        FundMe fundMe = new FundMe(priceFeed);
         vm.stopBroadcast();
-        return fundMe;
+        return (fundMe, helperConfig);
+    }
+
+    function run() external returns (FundMe, HelperConfig) {
+        return deployFundMe();
     }
 }
